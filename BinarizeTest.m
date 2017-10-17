89528c04814a9285482b9310e1450d43391f75c2
JP7FGD <jp7fgd@outlook.com>
2017-10-17 23:16:51 +0900

    Update

--- BinarizeTest.m
+++ BinarizeTest.m
@@ -9,13 +9,11 @@ Pixels = X_Pixels*Y_Pixels; % # of total pixels
 first_frame=1; % The specific Image you want to look at
 frames=1;
 fseek(fid2,(first_frame-1)*Pixels*2,'bof');
-I=zeros(Pixels,frames,'uint16');
-for n=1:1:frames
-    I(:,n)=(fread(fid2,Pixels,'uint16'));
-end
+I=zeros(Pixels,1,'uint16');
+I(:,1)=(fread(fid2,Pixels,'uint16'));
 fclose(fid2);
-N = [X_Pixels Y_Pixels frames]; % structure of data
-RawImages=permute(reshape(I,N),[2 1 3]);
+N = [X_Pixels Y_Pixels]; % structure of data
+RawImages=permute(reshape(I,N),[2 1]);
 ImageData = RawImages(400:700,480:800); % change acording to your need
 level = graythresh(ImageData) % Threshold value
 BW = imbinarize(ImageData,level);
