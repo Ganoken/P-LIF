b650474aa01ad81ee839176c061b722836dcbe3b
JP7FGD <jp7fgd@outlook.com>
2017-10-19 00:42:19 +0900

    Update

--- Binarize_Image.m
+++ Binarize_Image.m
@@ -1,16 +1,22 @@
 %Code for Binarizing large number of image(Intended for .mraw).
 %Sujoy  created 2017/10
 
-close all; clear BI; 
+%Initialize
+close all; clear BinarizeImage;
+%Load Data 
 FileID = fopen(sprintf(''),'r'); %input file
+%Set parameters
 X_Pixels = 1024;
 Y_Pixels = 1024;
 Pixels = X_Pixels*Y_Pixels; % # of total pixels/image 
 N = [X_Pixels Y_Pixels]; % structure of data
+%Generate needed matrix
 I=zeros(Pixels,1,'uint16'); % Pre make the matrix to hold the image
-BI=zeros(301,321,2000); % Pre make output matrix
+BinarizeImage=zeros(301,321,2000); % Pre make output matrix
+%Set up loop parameters
 ji=1; % initial image
 jf=2000; % end image for first run
+%main loop
 for l=1:10 % Each loop will proccess 2000 images 
     s=1; % ID for store
     for j=ji:jf
@@ -21,12 +27,13 @@ for l=1:10 % Each loop will proccess 2000 images
         ImageData = RawImages(400:700,480:800);
         %level = graythresh(ImageData); % to obtain the th value
         %BI(:,:,s) = imbinarize(ImageData,level);
-        BI(:,:,s) = imbinarize(ImageData);
+        BinarizeImage(:,:,s) = imbinarize(ImageData);
         s=s+1;
     end
     ji=ji+2000; % To process next 2000 image
     jf=jf+2000; % same as above
     FileName = ['' num2str(l,'%03u') '.mat']; % output file name
-    save(FileName,'BI');  
+    save(FileName,'BinarizeImage');  
 end
+%finish up
 fclose(FileID); % close file
