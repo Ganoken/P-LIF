a31c43731167e7d6e7d945a5ceb34cf18ec55f78
JP7FGD <jp7fgd@outlook.com>
2017-11-17 23:48:08 +0900

    Update

--- Binarize_Image.m
+++ Binarize_Image.m
@@ -20,13 +20,11 @@ jf=2000; % end image for first run
 for l=1:10 % Each loop will proccess 2000 images 
     %s=1; % ID for store
     for j=ji:jf
-        first_frame=j;
-        fseek(FileID,(first_frame-1)*Pixels*2,'bof');
+        fseek(FileID,(j-1)*Pixels*2,'bof');
         I(:,1)=(fread(FileID,Pixels,'uint16'));
         RawImages=permute(reshape(I,N),[2 1]);
         %level(:,s) = graythresh(RawImages(400:700,480:800)); % to obtain the th value
         BinarizeImage(:,:,j-((l-1)*2000)) = imbinarize(RawImages(400:700,480:800));
-        %s=s+1;
     end
     ji=ji+2000; % To process next 2000 image
     jf=jf+2000; % same as above
