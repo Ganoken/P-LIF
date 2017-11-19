a371f419c076f33ddc862cb5ca1f36876ac46526
JP7FGD <jp7fgd@outlook.com>
2017-11-19 23:27:28 +0900

    Update

--- PhaseMean.m
+++ PhaseMean.m
@@ -21,23 +21,24 @@ for k=1:(length(TimeUse)-1)
     
 end
 
-FileName = ['../../LIF/BinarizeImage/20170104L_75_on_1_201701_1147' num2str(n,'%03u') '.mat'];
-BI = load(FileName);
+FileName = ['.mat'];
+load(FileName);
 
 MeanImage = zeros(301,321,PhaseDivision);
 PhaseImage = zeros(301,321,700); %700 is an arbitary number
 for j=1:PhaseDivision
+        m=1;
     for k=1:(length(TimeUse)-1)
         if Phase(k,1,j) == 0
             break
         end
         for l=1:Phase(k,2,j)
-            PhaseImage(:,:,m) = BI.BinarizeImage(:,:,Phase(k,1,j)+l-1);
+            PhaseImage(:,:,m) = BinarizeImage(:,:,Phase(k,1,j)+l-1);
+            m=m+1;
         end
     end
     SumImage = sum(PhaseImage,3);
     MeanImage(:,:,j) = SumImage/(m-1);
-    %MeanImage(:,:,j) = SumImage/length
 end
 
 FileName = '../../Output/MeanImage/20170104L_75_on_1_201701_1147mean.mat'; % output file name
