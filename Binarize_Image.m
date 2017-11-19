4f6cc56e0e0ff228c2d7b0889f99e47d9b6d0236
JP7FGD <jp7fgd@outlook.com>
2017-11-19 23:14:52 +0900

    Initial change

--- Binarize_Image.m
+++ Binarize_Image.m
@@ -3,33 +3,35 @@
 
 %Initialize
 close all; clear BinarizeImage;
+
 %Load Data 
 FileID = fopen(sprintf(''),'r'); %input file
+
 %Set parameters
 X_Pixels = 1024;
 Y_Pixels = 1024;
 Pixels = X_Pixels*Y_Pixels; % # of total pixels/image 
 N = [X_Pixels Y_Pixels]; % structure of data
+
 %Generate needed matrix
 I=zeros(Pixels,1,'uint16'); % Pre make the matrix to hold the image
-BinarizeImage=zeros(301,321,2000); % Pre make output matrix
+BinarizeImage=zeros(301,321,20000); % Pre make output matrix
+
 %Set up loop parameters
 ji=1; % initial image
-jf=2000; % end image for first run
-%main loop
-for l=1:10 % Each loop will proccess 2000 images 
-    %s=1; % ID for store
-    for j=ji:jf
-        fseek(FileID,(j-1)*Pixels*2,'bof');
-        I(:,1)=(fread(FileID,Pixels,'uint16'));
-        RawImages=permute(reshape(I,N),[2 1]);
-        %level(:,s) = graythresh(RawImages(400:700,480:800)); % to obtain the th value
-        BinarizeImage(:,:,j-((l-1)*2000)) = imbinarize(RawImages(400:700,480:800));
-    end
-    ji=ji+2000; % To process next 2000 image
-    jf=jf+2000; % same as above
-    FileName = ['' num2str(l,'%03u') '.mat']; % output file name
-    save(FileName,'BinarizeImage');  
+jf=20000; % end image 
+
+%main loop   
+for j=ji:jf
+    fseek(FileID,(j-1)*Pixels*2,'bof');
+    I=(fread(FileID,Pixels,'uint16'));
+    RawImages=permute(reshape(I,N),[2 1]);
+    %level(:,j) = graythresh(RawImages(400:700,480:800)); % to obtain the th value
+    BinarizeImage(:,:,j-((l-1)*2000)) = imbinarize(RawImages(400:700,480:800));
 end
+
+%Save
+FileName = ['.mat']; % output file name
+save(FileName,'BinarizeImage');  
 %finish up
 fclose(FileID); % close file
